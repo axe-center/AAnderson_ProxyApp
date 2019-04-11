b401472ad91295f87fca1e51e59732b8e55cff77
AndersAustin <23081361+AndersAustin@users.noreply.github.com>
2019-04-11 09:42:36 -0500

    added in all components minus product description

--- server/server.js
+++ server/server.js
@@ -2,40 +2,13 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const axios = require('axios');
-
+require('dotenv').config()
 
 
 app.use(express.json({urlencoded: true}));
 app.use(cors());
 app.use(express.static('dist'))
 
-app.get('/api/navbar/products', (req, res) => {
-    //get req to navbar
-    axios.get('http://ec2-52-14-74-144.us-east-2.compute.amazonaws.com/api/navbar/products')
-    .then(dataz => {
-        let placeholder = dataz.data;
-        res.send(placeholder);
-    })
-    .catch(err => {
-        console.log(err);
-    })
-
-
-})
-
-app.get('/api/carousel/products', (req, res) => {
-    //get req to carousel
-    axios.get('http://ec2-3-17-65-225.us-east-2.compute.amazonaws.com/api/carousel/products')
-    .then(dataz => {
-        let placeholder = dataz.data;
-        res.send(placeholder);
-    })
-    .catch(err => {
-        console.log(err);
-    })
-
-
-})
 
 
 app.listen(process.env.PORT || 3020, (err, res) => {
